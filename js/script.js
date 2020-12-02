55f3d31fd24882a711c10adb9f481a4cbdd8bd98
unknown <dvilelaportela@gmail.com>
2020-12-02 02:49:24 -0300

    Efeito Accordion no Faq com CSS e JS

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+const initTabNav = () => {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
 
-    const activeTab = (index) => {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
+        const activeTab = (index) => {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
     }
-
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
+initTabNav();
 
+const initAccordion = () => {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const ativo = 'ativo';
 
+    if (accordionList.length) {
+        accordionList[0].classList.add(ativo);
+        accordionList[0].nextElementSibling.classList.add(ativo);
 
+        function activeAccordion() {
+            this.classList.toggle(ativo);
+            this.nextElementSibling.classList.toggle(ativo);
+        }
 
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion();
 
 
 
