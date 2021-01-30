36fba547777423e29d9e713ee27c9002d6deeb68
unknown <dvilelaportela@gmail.com>
2021-01-30 01:10:02 -0300

    Separação do CSS e implementação do Modal JS

---
+++ js/modules/modal.js
@@ -0,0 +1,28 @@
+const initModal = () => {
+    const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+    const botaoFechar = document.querySelector('[data-modal="fechar"]');
+    const containerModal = document.querySelector('[data-modal="container"]');
+
+    if (botaoAbrir && botaoFechar && containerModal) {
+
+        const toggleModal = (event) => {
+            event.preventDefault();
+            containerModal.classList.toggle('ativo');
+        }
+        const cliqueForaModal = (event) => {
+            if (event.target === this) {
+                toggleModal(event);
+            }
+        }
+
+        botaoAbrir.addEventListener('click', toggleModal);
+        botaoFechar.addEventListener('click', toggleModal);
+        containerModal.addEventListener('click', cliqueForaModal);
+    }
+}
+
+
+
+export default initModal;
+
+
--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimaScroll from './modules/scroll-animacao.js';
 import initTabNav from './modules/tabnav.js';
 import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimaScroll();
 initTabNav();
 initAccordion();
+initModal();
 
 
 
