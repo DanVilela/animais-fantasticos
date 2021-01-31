b046008fd96be54f27ce2990724e60d98323205d
unknown <dvilelaportela@gmail.com>
2021-01-31 01:17:23 -0300

    Adição de Tooltip no mapa

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ import initAnimaScroll from './modules/scroll-animacao.js';
 import initTabNav from './modules/tabnav.js';
 import initAccordion from './modules/accordion.js';
 import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
 
 initScrollSuave();
 initAnimaScroll();
 initTabNav();
 initAccordion();
 initModal();
+initToolTip();
 
 
 
