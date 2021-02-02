4409c6f815d6ad1051e683ef647f1e22df007002
unknown <dvilelaportela@gmail.com>
2021-02-02 01:21:56 -0300

    Dropdown menu e Outsideclick

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initTabNav from './modules/tabnav.js';
 import initAccordion from './modules/accordion.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimaScroll();
@@ -11,6 +12,7 @@ initTabNav();
 initAccordion();
 initModal();
 initToolTip();
+initDropdownMenu();
 
 
 
